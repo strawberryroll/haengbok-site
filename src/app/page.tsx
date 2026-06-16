068f7ebbd6ca723d94f64523cab080ce709970f1
Ahyeon Choi <138025938+strawberryroll@users.noreply.github.com>
2026-06-16 11:19:12 +0900

    feat: 메인 페이지 Welcome 섹션 구현 (#24)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@/components/home/Hero';
 import Values from '@/components/home/Values';
+import Welcome from '@/components/home/Welcome';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Values />
+      <Welcome />
     </>
   );
 }
