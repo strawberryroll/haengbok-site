14b4076786e7163bd7ce897343f04a8b44ebd542
ahyeon <nuyha234@gmail.com>
2026-06-22 12:15:06 +0900

    feat: 메인 페이지 OG 메타데이터 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,19 +20,15 @@ const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 export const metadata: Metadata = {
   metadataBase: baseUrl ? new URL(baseUrl) : undefined,
   title: '행복한 교회',
-  description:
-    '행복한 교회에 오신 것을 환영합니다. 저희는 사랑과 희망을 나누는 공동체입니다.',
+  description: '행복한 교회 공식 홈페이지입니다.',
   openGraph: {
     title: '행복한 교회',
-    description:
-      '행복한 교회에 오신 것을 환영합니다. 저희는 사랑과 희망을 나누는 공동체입니다.',
+    description: '행복한 교회 공식 홈페이지입니다.',
     url: `${baseUrl}`,
     siteName: '행복한 교회',
     images: [
       {
         url: `${baseUrl}/images/church.jpeg`,
-        width: 1200,
-        height: 630,
       },
     ],
     locale: 'ko_KR',
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,24 @@ import Schedule from '@/components/home/Schedule';
 import Sermon from '@/components/home/Sermon';
 import Values from '@/components/home/Values';
 import Welcome from '@/components/home/Welcome';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '행복한 교회',
+  description:
+    '행복한 교회에 오신 것을 환영합니다. 저희는 사랑과 희망을 나누는 공동체입니다.',
+  openGraph: {
+    title: '행복한 교회',
+    description:
+      '행복한 교회에 오신 것을 환영합니다. 저희는 사랑과 희망을 나누는 공동체입니다.',
+    siteName: '행복한 교회',
+    images: [
+      {
+        url: '/images/church.jpeg',
+      },
+    ],
+  },
+};
 
 export default function Home() {
   return (
