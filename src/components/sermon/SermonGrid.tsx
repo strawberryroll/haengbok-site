5893370a55f3c4ae52e8af81821a3ba0581f77fb
ahyeon <nuyha234@gmail.com>
2026-08-04 18:48:47 +0900

    style: 카드 그리드 좌우 여백 반응형 조정

--- src/components/sermon/SermonGrid.tsx
+++ src/components/sermon/SermonGrid.tsx
@@ -21,7 +21,7 @@ export default function SermonGrid({ sermons }: SermonGridProps) {
   const hasMore = visibleCount < sermons.length;
 
   return (
-    <div className="px-15 pb-20">
+    <div className="px-8 pb-20 lg:px-15">
       <div className="grid grid-cols-1 gap-x-10 gap-y-8 sm:grid-cols-2 lg:grid-cols-3">
         {visibleSermons.map((sermon) => (
           <SermonCard
